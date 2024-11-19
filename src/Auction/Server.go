f5ede4e971d308079f7f8a8185abc042fffb0005
Jon <jble@itu.dk>
2024-11-19 13:24:12 +0100

    Fleshing out Auction in server.go

--- src/Auction/Server.go
+++ src/Auction/Server.go
@@ -1,18 +1,64 @@
 package main
 
-func Bid(bid Amount) {
-	//if (!leader) {return response from leader}
+import (
+	"Mutual_Exclusion/m/v2/raalgo"
+	pb "Mutual_Exclusion/m/v2/raalgo"
+	"context"
+	"log"
+	"sync"
+)
+
+type P2PNode struct {
+	pb.UnimplementedAuctionServer
+	peers             map[string]pb.AuctionClient // map of peer addresses to clients
+	peerLock          sync.RWMutex
+	leader            pb.AuctionClient
+	Highest_Bid       int64
+	Highest_BidId     int64
+	Our_Timestamp     int64
+	Highest_Timestamp int64
+	IsLeader          bool
+	peerPorts         []string
 }
 
-func Status() (result int) {
+func (n *P2PNode) Bid(bid pb.Amount) *raalgo.Ack {
+	if !n.IsLeader {
+		ack, _ := n.leader.Bid(bid)
+		return ack
+	}
+	//if (!leader) {return response from leader}
+	if bid.Amount > n.Highest_Bid {
+		UpdateFollowers(bid)
+		n.Highest_Bid = bid.Amount
+		n.Highest_BidId = bid.Id
+		n.Highest_Timestamp = n.Highest_Timestamp + 1
+		return &pb.Ack{
+			ack: true,
+			id:  n.Highest_BidId,
+		}
+	}
+}
+
+func result() (outcome int) {
 	//return highest value
 }
 
-func UpdateFollowers() {
-	//Send message to followers with the updated value
+func (n *P2PNode) UpdateFollowers(update pb.Amount) {
+	n.peerLock.RLock()
+	for address := range n.peers {
+		if peer, exists := n.peers[address]; exists {
+			_, err := peer.Update(context.Background(), update)
+			if err != nil {
+				log.Printf("Error sending message: %v", err)
+			}
+		}
+		//Send message to followers with the updated value
+
+	}
+	n.peerLock.RUnlock()
 }
 
-func Update() {
+func (n *P2PNode) Update(ctx context.Context, update pb.Amount) {
 	//Call from leader to update value
 }
 
