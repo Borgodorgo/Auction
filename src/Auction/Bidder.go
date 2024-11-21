42cdf9c39dfa60fbf7d22a58c1e9b4f90537476d
Jon <jble@itu.dk>
2024-11-21 12:19:30 +0100

    Renamed Client to Bidder Wrote initial logic for Bidder actions.

---
+++ src/Auction/Bidder.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	as "Mutual_Exclusion/m/v2/raalgo"
+	"context"
+	"log"
+	"math/rand/v2"
+	"sync"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Bidder struct {
+	as.UnimplementedAuctionServer
+	AuctionContact as.AuctionClient
+	MyLatestBid    int64
+	Id             int64
+
+	peers         map[string]pb.P2PnetworkClient // map of peer addresses to clients
+	peerLock      sync.RWMutex
+	ME            int64
+	N             int64
+	Our_Timestamp int64
+}
+
+func (bidder *Bidder) Bid(amount int64) {
+	bidder.AuctionContact.Bid(context.Background(), &as.Amount{
+		Amount: amount,
+		Id:     bidder.Id,
+	})
+}
+
+func (bidder *Bidder) Status() {
+	auctionStatus, err := bidder.AuctionContact.Result(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		FindNode()
+		//bidder.Status()
+	}
+	if auctionStatus.Amount != bidder.MyLatestBid {
+		bidder.MyLatestBid = auctionStatus.Amount + 10
+		bidder.Bid(bidder.MyLatestBid)
+	}
+}
+
+func FindNode() {
+	nodes := []string{"5001", "5002", "5003", "5004", "5005"}
+
+	for {
+		randomNumber := rand.Int64N(4)
+		address := nodes[randomNumber]
+		conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Printf("Failed to connect to peer %s: %v", address, err)
+			continue
+		}
+
+		client := pb.NewP2PnetworkClient(conn)
+		n.peerLock.Lock()
+		n.peers[address] = client
+		n.peerPorts[nodeid] = address
+		n.peerLock.Unlock()
+
+		log.Printf("Connected to node %s", address)
+
+	}
+
+}
