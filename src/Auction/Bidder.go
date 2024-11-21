e47c3a9c487e985c898bb05d047171a4b33bd787
Jon <jble@itu.dk>
2024-11-21 13:23:46 +0100

    Initial testing of Bidder

--- src/Auction/Bidder.go
+++ src/Auction/Bidder.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	as "Mutual_Exclusion/m/v2/raalgo"
+	as "Replication/m/v2/AuctionService/Auction"
 	"context"
 	"log"
 	"math/rand/v2"
-	"sync"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -13,33 +12,31 @@ import (
 )
 
 type Bidder struct {
-	as.UnimplementedAuctionServer
-	AuctionContact as.AuctionClient
+	as.UnimplementedAuctionServiceServer
+	AuctionContact as.AuctionServiceClient
 	MyLatestBid    int64
 	Id             int64
 	nodes          []string
-
-	peers         map[string]pb.P2PnetworkClient // map of peer addresses to clients
-	peerLock      sync.RWMutex
-	ME            int64
-	N             int64
-	Our_Timestamp int64
 }
 
 func (bidder *Bidder) Bid(amount int64) {
+	log.Printf("Bidding %d", amount)
 	bidder.AuctionContact.Bid(context.Background(), &as.Amount{
-		Amount: amount,
-		Id:     bidder.Id,
+		Amount:   amount,
+		Bidderid: bidder.Id,
 	})
 }
 
 func (bidder *Bidder) Status() {
 	auctionStatus, err := bidder.AuctionContact.Result(context.Background(), &emptypb.Empty{})
 	if err != nil {
-		FindNode()
+		log.Printf("Failed to get auction status: %v", err)
+		bidder.FindNode()
 		//bidder.Status()
 	}
+
 	if auctionStatus.Amount != bidder.MyLatestBid {
+		log.Printf("Auction status: %d", auctionStatus.Amount)
 		bidder.MyLatestBid = auctionStatus.Amount + 10
 		bidder.Bid(bidder.MyLatestBid)
 	}
@@ -48,27 +45,31 @@ func (bidder *Bidder) Status() {
 func (bidder *Bidder) FindNode() {
 	for {
 		randomNumber := rand.Int64N(4)
-		address := bidder.nodes[randomNumber]
+		address := "localhost:" + bidder.nodes[randomNumber]
+		log.Print(randomNumber)
+		log.Print(address)
 		conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
 			log.Printf("Failed to connect to peer %s: %v", address, err)
 			continue
 		}
 
-		client := pb.NewP2PnetworkClient(conn)
-		n.peerLock.Lock()
-		n.peers[address] = client
-		n.peerPorts[nodeid] = address
-		n.peerLock.Unlock()
-
+		client := as.NewAuctionServiceClient(conn)
+		bidder.AuctionContact = client
 		log.Printf("Connected to node %s", address)
-
+		return
 	}
 }
+
 func start() {
 	bidder := Bidder{
-		nodes: []string{"5001", "5002", "5003", "5004", "5005"},
+		nodes:       []string{"5001", "5002", "5003", "5004", "5005"},
+		MyLatestBid: -1,
 	}
+
 	bidder.FindNode()
-	bidder.Status()
+	log.Print("Found node")
+	for {
+		bidder.Status()
+	}
 }
--- src/Auction/main.go
+++ src/Auction/main.go
@@ -1,41 +1,56 @@
 package main
 
-import (
-	"time"
-)
+import "time"
 
-func main() {
-	node1 := CreateNode(0, 3)
-	node2 := CreateNode(1, 3)
-	node3 := CreateNode(2, 3)
+/*
+	func oldmain() {
+		node1 := CreateNode(0, 3)
+		node2 := CreateNode(1, 3)
+		node3 := CreateNode(2, 3)
 
-	// Start the gRPC server
-	go StartServer(node1, ":50051")
-	go StartServer(node2, ":50052")
-	go StartServer(node3, ":50053")
+		// Start the gRPC server
+		go StartServer(node1, ":50051")
+		go StartServer(node2, ":50052")
+		go StartServer(node3, ":50053")
 
-	time.Sleep(3 * time.Second)
+		time.Sleep(3 * time.Second)
 
-	// Add peers (simulate peer discovery for demonstration)
-	node1.AddPeer("localhost:50052", 1)
-	node1.AddPeer("localhost:50053", 2)
+		// Add peers (simulate peer discovery for demonstration)
+		node1.AddPeer("localhost:50052", 1)
+		node1.AddPeer("localhost:50053", 2)
 
-	node2.AddPeer("localhost:50051", 0)
-	node2.AddPeer("localhost:50053", 2)
+		node2.AddPeer("localhost:50051", 0)
+		node2.AddPeer("localhost:50053", 2)
 
-	node3.AddPeer("localhost:50051", 0)
-	node3.AddPeer("localhost:50052", 1)
+		node3.AddPeer("localhost:50051", 0)
+		node3.AddPeer("localhost:50052", 1)
 
-	go node1.Start()
-	go node2.Start()
-	go node3.Start()
+		go node1.Start()
+		go node2.Start()
+		go node3.Start()
 
-	counter := 0
-	for {
-		if counter > 100 {
-			break
+		counter := 0
+		for {
+			if counter > 100 {
+				break
+			}
+			time.Sleep(time.Second)
+			counter++
 		}
-		time.Sleep(time.Second)
-		counter++
 	}
+*/
+func main() {
+
+	node1 := CreateNode()
+	node2 := CreateNode()
+	node3 := CreateNode()
+	node4 := CreateNode()
+	node5 := CreateNode()
+	go startServer(node1, ":5001")
+	go startServer(node2, ":5002")
+	go startServer(node3, ":5003")
+	go startServer(node4, ":5004")
+	go startServer(node5, ":5005")
+	time.Sleep(3 * time.Second)
+	start()
 }
